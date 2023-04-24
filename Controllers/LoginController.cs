f493cfdc2b963aead7842347fdf0fe842d46e260
Gustavo Passos <gustavofsp@hotmail.com>
2023-04-24 14:06:17 -0300

    Login Controller and Testing

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,36 @@
+﻿using System.Threading.Tasks;
+using ApiAuth.Models;
+using ApiAuth.Repositories;
+using ApiAuth.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiAuth.Controllers
+{
+    [ApiController]
+    [Route("v1")]
+    public class LoginController : ControllerBase
+    {
+        [HttpPost]
+        [Route("login")]
+        public async Task<ActionResult<dynamic>> AuthenticateAsync([FromBody] User model)
+        {
+            var user = UserRepository.Get(model.UserName, model.Password);
+            if (user == null)
+            {
+                return NotFound(new { message = "Usuario ou senha invalidos" });
+            }
+
+            var token = TokenService.GenerateToken(user);
+            user.Password = "";
+
+            return new
+            {
+                user = user,
+                token = token
+
+            };
+
+
+        }
+    }
+}
